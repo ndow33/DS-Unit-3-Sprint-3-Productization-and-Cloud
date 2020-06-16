17ebfc34991649580fce24dae5c0a7933f5b3095
Nate Dow <55363203+ndow33@users.noreply.github.com>
2020-06-15 20:04:45 -0600

    Add files via upload

--- sprint/DS-Unit-3-Sprint-3-Productization-and-Cloud/sprint/aq_dashboard.py
+++ sprint/DS-Unit-3-Sprint-3-Productization-and-Cloud/sprint/aq_dashboard.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import openaq
 import requests
-# Create an instance of the OpenAQ API
+# connects to the api
 api = openaq.OpenAQ()
 
 # initialize the app
@@ -11,8 +11,6 @@ APP = Flask(__name__)
 # configure the database
 APP.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 DB = SQLAlchemy(APP)
-
-############### PART 2 - BREATHE EASY WITH OPENAQ ##########################
 
 # define a function that will return a list of date time and value tuples
 def tuple_list():
@@ -31,8 +29,6 @@ def tuple_list():
     
   return test_list
 
-
-
 # Base route
 @APP.route('/')
 def root():
@@ -40,55 +36,21 @@ def root():
     test = tuple_list()    
     return str(test)
 
-####################### PART 4 - DASHBOARD TO THE FINISH ################################
-
-def parse_records(database_records):
-    parsed_records = []
-    for record in database_records:
-        print(record)
-        parsed_record = str(record) #.__dict__
-        # del parsed_record["_sa_instance_state"]
-        parsed_records.append(parsed_record)
-    return parsed_records
-
-# Call data from the database to the /dashboard page
-@APP.route('/dashboard')
-def dashboard():
-
-    records = Record.query.filter(Record.value>=10).all()
-    
-    return str(parse_records(records))
-
-
-
-###################### PART 3 - THE DATA BELONGS IN A MODEL #################################
-
 class Record(DB.Model):
     id = DB.Column(DB.Integer, primary_key=True)
     datetime = DB.Column(DB.String(25))
     value = DB.Column(DB.Float, nullable=False)
 
     def __repr__(self):
-        return f'Time: {self.datetime}-------Value: {self.value}'
+        return 'TODO - write a nice representation of Records'
 
-# sends data from an API endpoint to a SQLite database
 @APP.route('/refresh')
 def refresh():
     """Pull fresh data from Open AQ and replace existing data."""
     DB.drop_all()
     DB.create_all()
     # TODO Get data from OpenAQ, make Record objects with it, and add to db
+    db_list = tuple_list()
 
-    test = tuple_list()
-    counter = 0
-    for x in test:
-
-      new_record = Record(datetime=test[counter][0], value=test[counter][1])
-      DB.session.add(new_record)
-      print(new_record.datetime)
-      print(new_record.value)
-      print(new_record)
-      counter = counter + 1
-    
     DB.session.commit()
     return 'Data refreshed!'
